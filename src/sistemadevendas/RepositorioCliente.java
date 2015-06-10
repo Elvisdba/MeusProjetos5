14f37450b69d3dc07d11ccd4ab5b791b3bcbbe97
Gleidson <bob_glei@hotmail.com>
2015-06-10 18:50:12 -0300

    Repositório do cliente feito

--- src/sistemadevendas/RepositorioCliente.java
+++ src/sistemadevendas/RepositorioCliente.java
@@ -29,20 +29,60 @@ public class RepositorioCliente {
         if (cliente == null) {
             throw new Exception("O cliente não foi instanciado");
         }
-        if (cliente .getCpf() == null) {
+        if (cliente.getCpf() == null) {
+            throw new Exception ("Informar o Cpf do cliente");
+        }
+        if (cliente.getCpf().trim().equals("")) {
             throw new Exception ("Informar o Cpf do cliente");
         }
         if (cliente.getNome() == null) {
             throw new Exception("Informar o nome do cliente");
         }
+        if (cliente.getNome().trim().equals("")) {
+            throw new Exception("Informar o nome do cliente");
+        }
+        if (this.verificarExistencia(cliente) >= 0){
+            throw new Exception("O referido cliente já se encontra cadastrado");
+        }
+        this.lista.add(cliente);
     }
     
     public void remover (Cliente cliente) throws Exception {
-    
+        
+        if (cliente == null){
+            throw new Exception("O cliente não foi instanciado");
+        }
+        if(cliente.getCpf() == null){
+            throw new Exception("Informar o CPF do cliente");
+        }
+        if (cliente.getCpf().trim().equals("")){
+            throw new Exception("Informar o CPF do cliente");
+        }
+        if (this.verificarExistencia(cliente) == -1){
+            throw new Exception("O referido cliente não está cadastrado");
+        }
+        this.lista.remove(this.verificarExistencia(cliente));
     }
     
     public void atualizar (Cliente cliente) throws Exception {
-    
+        if (cliente == null){
+            throw new Exception("O cliente não foi instanciado");
+        }
+        if(cliente.getCpf() == null){
+            throw new Exception("Informar o CPF do cliente");
+        }
+        if (cliente.getCpf().trim().equals("")){
+            throw new Exception("Informar o CPF do cliente");
+        }
+        if (cliente.getNome() == null){
+            throw new Exception("Informar o Nome do cliente");
+        }
+        if (cliente.getNome().trim().equals("")){
+            throw new Exception("Informar o Nome do cliente");
+        }
+        this.lista.set(this.verificarExistencia(cliente), cliente);
+        
+        
     }
     
     public int verificarExistencia (Cliente cliente){
