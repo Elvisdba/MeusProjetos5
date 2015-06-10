64c9eb9c234b3c7764544fb1d888cd3a9206f6b7
Gleidson <bob_glei@hotmail.com>
2015-06-10 19:12:08 -0300

    Repositório do produto feito

---
+++ src/sistemadevendas/RepositorioProduto.java
@@ -0,0 +1,100 @@
+
+package sistemadevendas;
+
+import java.util.ArrayList;
+
+public class RepositorioProduto {
+    
+    private ArrayList<Produto> lista;
+    private static RepositorioProduto instanciaRep;
+    
+    public RepositorioProduto(){
+        this.lista = new ArrayList<Produto>();
+    }
+    public static RepositorioProduto obterInstancia() {
+        if(instanciaRep == null){
+            instanciaRep = new RepositorioProduto();
+        }
+        return instanciaRep;
+    }
+    public ArrayList<Produto> listarTodos(){
+       return this.lista;
+    }
+    public void inserir(Produto produto) throws Exception{
+        if (produto == null) {
+            throw new Exception("O cliente não foi instanciado");
+        }
+        if (produto.getCodigodebarras() == null) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (produto.getCodigodebarras().trim().equals("")) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (produto.getDescricao()== null) {
+            throw new Exception("Informar a descrição do produto");
+        }
+        if (produto.getDescricao().trim().equals("")) {
+            throw new Exception("Informar a descrição do produto");
+        }
+        if (produto.getPreco() <= 0) {
+            throw new Exception("O preço do produto deverá ser superior a zero");
+        }
+        if (this.verificaExistencia(produto) >= 0) {
+            throw new Exception("O referido produto já se encontra cadastrado");
+        }
+        this.lista.add(produto);
+        
+        
+    }
+    public void remover(Produto produto) throws Exception{
+        if (produto == null) {
+            throw new Exception("O cliente não foi instanciado");
+        }
+        if (produto.getCodigodebarras() == null) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (produto.getCodigodebarras().trim().equals("")) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (this.verificaExistencia(produto) == -1) {
+            throw new Exception("O referido produto não se encontra cadastrado");
+        }
+        this.lista.remove(this.verificaExistencia(produto));
+    }
+    public void atualizar(Produto produto) throws Exception{
+        if (produto == null) {
+            throw new Exception("O cliente não foi instanciado");
+        }
+        if (produto.getCodigodebarras() == null) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (produto.getCodigodebarras().trim().equals("")) {
+            throw new Exception("Informar o código de barras do produto");
+        }
+        if (produto.getDescricao()== null) {
+            throw new Exception("Informar a descrição do produto");
+        }
+        if (produto.getDescricao().trim().equals("")) {
+            throw new Exception("Informar a descrição do produto");
+        }
+        if (produto.getPreco() <= 0) {
+            throw new Exception("O preço do produto deverá ser superior a zero");
+        }
+        if (this.verificaExistencia(produto) < 0) {
+            throw new Exception("O referido produto não se encontra cadastrado");
+        }
+        this.lista.set(this.verificaExistencia(produto), produto);
+    }
+    public int verificaExistencia(Produto produto){
+        int retorno = -1;
+        for (int i = 0; i < this.lista.size(); i++){
+            if (produto.getCodigodebarras().trim().equals(this.lista.get(i).getCodigodebarras().trim())){
+                retorno = i;
+                break;
+            }
+        }
+        return retorno;
+    
+    }
+    
+}
